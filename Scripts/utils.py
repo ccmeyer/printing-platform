32650af546b73673690a77ce4f4b0fcb19a9b204
ccmeyer <conarymeyer@gmail.com>
2021-11-03 15:49:46 -0700

    Added the quit option to select options

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -23,13 +23,21 @@ def select_options(lst,message='Select one of the options:', trim=False):
                 simple_paths.append(l.replace('\\','/').split('/')[-1])
     else:
         simple_paths = lst
+    exit_option = 'Exit'
+    lst.append(exit_option)
+    simple_paths.append(exit_option)
+
     for index,opt in enumerate(simple_paths):
         print(index,opt)
     enteredStr = input(message)
     while not enteredStr.isnumeric() or int(enteredStr) >= len(lst) or int(enteredStr) < 0:
         enteredStr = input("The entered command is invalid. Please enter a valid index: ")
     print('Option selected: ',lst[int(enteredStr)],'\n')
-    return lst[int(enteredStr)]
+    if lst[int(enteredStr)] == exit_option:
+        quit = True
+    else:
+        quit = False
+    return lst[int(enteredStr)],quit
 
 def ask_for_number(message='Enter number: '):
     try:
